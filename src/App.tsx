1abf3c2f5f0ed702ab11e0228a83955e81292b83
IgnaOvidiu <108483395+IgnaOvidiu@users.noreply.github.com>
2022-10-09 11:07:34 +0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ interface Country {
   capital: string;
   population: number;
   isEUMember: boolean;
+  flag: string;
 }
 
 function App() {
@@ -15,36 +16,41 @@ function App() {
       capital: "Bucharest",
       population: 1800000,
       isEUMember: true,
+      flag: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAP8AAACqCAMAAABVlWm8AAAACVBMVEUAK3/80RbOESaEDa1tAAAAxklEQVR4nO3PMQEAAAjAILV/aFPsgwbMFLZwhaTv7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v7+/v75/+HwW0qVdTeFEnAAAAAElFTkSuQmCC",
     },
     {
       name: "Ukraine",
       capital: "Kiev",
       population: 3500000,
       isEUMember: false,
+      flag: "https://rags2riches4schools.co.uk/wp-content/uploads/2022/02/Ukraine-Flag.png",
     },
     {
       name: "Germany",
       capital: "Berlin",
       population: 8000000,
       isEUMember: true,
+      flag: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png",
     },
     {
       name: "Italy",
       capital: "Rome",
       population: 4500000,
       isEUMember: true,
+      flag: "https://wallpaperaccess.com/full/46396.jpg",
     },
     {
       name: "Jordan",
       capital: "Amman",
       population: 10000000,
       isEUMember: false,
+      flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Flag_of_Jordan.svg/2560px-Flag_of_Jordan.svg.png",
     },
   ];
 
   return (
-    <div className="App">
-      <div className="card">
+    <div className="App container mt-6">
+      <div className="card grid grid-cols-5 gap-4 text-center">
         {list.map((country) => (
           <Country props={country} key={`${country.name}`} />
         ))}
--- src/components/country.tsx
+++ src/components/country.tsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
 
 const Country = ({ props }) => {
-  useEffect(() => {
-    console.log("ce se intampla");
-  }, []);
   return (
-    <div key={`${props.name}`}>
-      <h1>{props.name}</h1>
-      <h3>{props.capital}</h3>
-      <span>{props.population}</span>
+    <div key={`${props.name}`} className="country flex-col items-center">
+      <img src={props.flag} alt="" />
+      <h1>Country: {props.name}</h1>
+      <h3>Capital: {props.capital}</h3>
+      <span>Population: {props.population}</span>
       {props.isEUMember ? <p>✅ Is Eu Member</p> : <p> ❌ Not EU Member</p>}
     </div>
   );
